6d953331bfab9cbf0d136621a3518187da60c0cb
Duane <duaneh.personal@gmail.com>
2026-05-10 15:57:17 -0600

    refactor(ui): move rowskeleton structural styles to CSS file

--- src/components/RowSkeleton.tsx
+++ src/components/RowSkeleton.tsx
@@ -1,15 +1,16 @@
+import './RowSkeleton.css';
 import { Skeleton } from './Skeleton';
 
 export function RowSkeleton() {
   return (
     <section>
-      <div style={{ display: 'flex', alignItems: 'baseline', marginBottom: 18 }}>
+      <div className="row-skeleton__header">
         <Skeleton width={220} height={26} radius={4} />
       </div>
-      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(7, 1fr)', gap: 18 }}>
+      <div className="row-skeleton__grid">
         {Array.from({ length: 7 }).map((_, i) => (
-          <div key={i} style={{ position: 'relative', width: '100%', paddingTop: '56.25%' }}>
-            <div data-skeleton style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, borderRadius: 8 }} />
+          <div key={i} className="row-skeleton__cell">
+            <div data-skeleton className="row-skeleton__cell-fill" />
           </div>
         ))}
       </div>
